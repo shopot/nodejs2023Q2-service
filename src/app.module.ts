804aeb43be45b6fcebd84570a85cc1000ffdfdab
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-26 19:26:21 +0300

    feat: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [UserModule, ArtistModule],
+  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavoriteModule],
 })
 export class AppModule {}

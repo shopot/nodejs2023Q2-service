ce2ec9a61deffcf59938bb851fe96fa766dd9c19
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-26 19:24:53 +0300

    feat: add artist module

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  HttpCode,
+} from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { UuidDto } from '../common/dto';
 
 @Controller('artist')
 export class ArtistController {
@@ -18,17 +28,18 @@ export class ArtistController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.artistService.findOne(+id);
+  findOne(@Param() { id }: UuidDto) {
+    return this.artistService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
-    return this.artistService.update(+id, updateArtistDto);
+  @Put(':id')
+  update(@Param() { id }: UuidDto, @Body() updateArtistDto: UpdateArtistDto) {
+    return this.artistService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.artistService.remove(+id);
+  @HttpCode(204)
+  remove(@Param() { id }: UuidDto) {
+    return this.artistService.remove(id);
   }
 }
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,26 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { BaseRepository } from '../common/database/base-repository';
+import { Artist } from './entities/artist.entity';
+import { NotFoundException } from '../common/exceptions';
 
 @Injectable()
 export class ArtistService {
-  create(createArtistDto: CreateArtistDto) {
-    return 'This action adds a new artist';
+  private artistDb: BaseRepository<Artist>;
+
+  constructor() {
+    this.artistDb = new BaseRepository<Artist>();
   }
 
-  findAll() {
-    return `This action returns all artist`;
+  create(createArtistDto: CreateArtistDto): Artist {
+    const artist = new Artist({
+      id: uuidv4(),
+      ...createArtistDto,
+    });
+
+    return this.artistDb.create(artist);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} artist`;
+  findAll(): Artist[] {
+    return this.artistDb.find();
   }
 
-  update(id: number, updateArtistDto: UpdateArtistDto) {
-    return `This action updates a #${id} artist`;
+  findOne(id: string): Artist {
+    const foundArtist = this.artistDb.findOneBy({ id });
+
+    if (foundArtist === null) {
+      throw new NotFoundException();
+    }
+
+    return foundArtist;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} artist`;
+  update(id: string, { name, grammy }: UpdateArtistDto) {
+    const foundArtist = this.artistDb.findOneBy({ id });
+
+    if (foundArtist === null) {
+      throw new NotFoundException();
+    }
+
+    const updatedArtist = new Artist({ ...foundArtist, name, grammy });
+
+    return this.artistDb.update(id, updatedArtist);
+  }
+
+  remove(id: string) {
+    if (!this.artistDb.has(id)) {
+      throw new NotFoundException();
+    }
+
+    return this.artistDb.remove({ id });
   }
 }

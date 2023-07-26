130b048a327bf304295cbc1f24f1819795b90721
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-26 19:25:37 +0300

    feat: add track module

---
+++ src/track/track.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  HttpCode,
+  Put,
+} from '@nestjs/common';
+import { TrackService } from './track.service';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { UpdateTrackDto } from './dto/update-track.dto';
+import { UuidDto } from '../common/dto';
+
+@Controller('track')
+export class TrackController {
+  constructor(private readonly trackService: TrackService) {}
+
+  @Post()
+  create(@Body() createTrackDto: CreateTrackDto) {
+    return this.trackService.create(createTrackDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.trackService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param() { id }: UuidDto) {
+    return this.trackService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param() { id }: UuidDto, @Body() updateTrackDto: UpdateTrackDto) {
+    return this.trackService.update(id, updateTrackDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param() { id }: UuidDto) {
+    return this.trackService.remove(id);
+  }
+}

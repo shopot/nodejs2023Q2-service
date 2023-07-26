57bde88c8f75c581bebf5e74b3a504b7c4bc7890
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-26 11:44:31 +0300

    refactor: add port and log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const PORT = 4000;
+
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
-}
-bootstrap();
+
+  await app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+  });
+};
+
+void bootstrap();

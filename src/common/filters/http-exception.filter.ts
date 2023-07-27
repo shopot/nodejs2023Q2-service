13bba90e1cf6af74996765cc19095cf633e69096
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-27 11:11:23 +0300

    feat: add error message

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -16,6 +16,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
 
     response.status(status).json({
       statusCode: status,
+      error: exception.getResponse() || exception.message,
       timestamp: new Date().toISOString(),
       path: request.url,
     });

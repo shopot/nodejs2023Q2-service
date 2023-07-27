243edc44916af058d351ee2327db74ab0d63fe5b
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-27 11:09:54 +0300

    fix: remove console log

--- src/common/interceptors/transform.interceptor.ts
+++ src/common/interceptors/transform.interceptor.ts
@@ -15,7 +15,6 @@ export class TransformInterceptor implements NestInterceptor {
   ): Observable<any> {
     return next.handle().pipe(
       map((data) => {
-        console.log(data);
         return instanceToPlain(data);
       }),
     );

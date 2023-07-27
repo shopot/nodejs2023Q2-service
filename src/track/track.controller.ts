1b60f2f0d25dce56dab5b8fc453304fe01a4818d
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-27 13:43:33 +0300

    feat: implement track functionality

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -7,13 +7,26 @@ import {
   Delete,
   HttpCode,
   Put,
+  UseFilters,
+  UseInterceptors,
 } from '@nestjs/common';
+import { StatusCodes } from 'http-status-codes';
+
 import { TrackService } from './track.service';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
 import { UuidDto } from '../common/dto';
+import { HttpExceptionFilter } from '../common/filters';
+import { TransformInterceptor } from '../common/interceptors';
+import {
+  HttpNotFoundException,
+  HttpServerErrorException,
+  NotFoundErrorException,
+} from '../common/exceptions';
 
 @Controller('track')
+@UseFilters(new HttpExceptionFilter())
+@UseInterceptors(new TransformInterceptor())
 export class TrackController {
   constructor(private readonly trackService: TrackService) {}
 
@@ -29,17 +42,41 @@ export class TrackController {
 
   @Get(':id')
   findOne(@Param() { id }: UuidDto) {
-    return this.trackService.findOne(id);
+    try {
+      return this.trackService.findOne(id);
+    } catch (err) {
+      if (err instanceof NotFoundErrorException) {
+        throw new HttpNotFoundException();
+      }
+
+      throw new HttpServerErrorException();
+    }
   }
 
   @Put(':id')
   update(@Param() { id }: UuidDto, @Body() updateTrackDto: UpdateTrackDto) {
-    return this.trackService.update(id, updateTrackDto);
+    try {
+      return this.trackService.update(id, updateTrackDto);
+    } catch (err) {
+      if (err instanceof NotFoundErrorException) {
+        throw new HttpNotFoundException();
+      }
+
+      throw new HttpServerErrorException();
+    }
   }
 
   @Delete(':id')
-  @HttpCode(204)
+  @HttpCode(StatusCodes.NO_CONTENT)
   remove(@Param() { id }: UuidDto) {
-    return this.trackService.remove(id);
+    try {
+      return this.trackService.remove(id);
+    } catch (err) {
+      if (err instanceof NotFoundErrorException) {
+        throw new HttpNotFoundException();
+      }
+
+      throw new HttpServerErrorException();
+    }
   }
 }
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,26 +1,67 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import { DatabaseService } from '../database/database.service';
+import { Track } from './entities/track.entity';
+import { NotFoundErrorException } from '../common/exceptions';
 
 @Injectable()
 export class TrackService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
   create(createTrackDto: CreateTrackDto) {
-    return 'This action adds a new track';
+    const { name, duration, artistId, albumId } = createTrackDto;
+
+    const track = new Track({
+      id: uuidv4(),
+      name,
+      artistId: artistId || null,
+      albumId: albumId || null,
+      duration,
+    });
+
+    return this.databaseService.tracks.create(track);
   }
 
   findAll() {
-    return `This action returns all track`;
+    return this.databaseService.tracks.find();
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} track`;
+    const foundTrack = this.databaseService.tracks.findOneBy({ id });
+
+    if (foundTrack === null) {
+      throw new NotFoundErrorException();
+    }
+
+    return foundTrack;
   }
 
   update(id: string, updateTrackDto: UpdateTrackDto) {
-    return `This action updates a #${id} track`;
+    const foundTrack = this.databaseService.tracks.findOneBy({ id });
+
+    if (foundTrack === null) {
+      throw new NotFoundErrorException();
+    }
+
+    const updatedTrack = new Track({
+      ...foundTrack,
+      ...updateTrackDto,
+    });
+
+    return this.databaseService.tracks.update(id, updatedTrack);
   }
 
   remove(id: string) {
-    return `This action removes a #${id} track`;
+    if (!this.databaseService.tracks.has(id)) {
+      throw new NotFoundErrorException();
+    }
+
+    // Remove from favorites
+    this.databaseService.favorites.tracks.delete(id);
+
+    return this.databaseService.tracks.remove({ id });
   }
 }

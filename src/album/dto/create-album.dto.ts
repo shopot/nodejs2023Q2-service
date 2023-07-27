6867ffa12b559b230cee3a7e01d62c3f94661bd3
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-27 13:43:02 +0300

    feat: implement album endpoint

--- src/album/dto/create-album.dto.ts
+++ src/album/dto/create-album.dto.ts
@@ -1,1 +1,15 @@
-export class CreateAlbumDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  year: number;
+
+  @IsString()
+  @IsOptional()
+  artistId?: string | null;
+}
--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,1 +1,18 @@
-export class CreateTrackDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+export class CreateTrackDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  artistId?: string;
+
+  @IsOptional()
+  @IsString()
+  albumId?: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  duration: number;
+}

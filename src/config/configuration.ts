9c5d8b7d85977deb318ad8662c6cb10a41e23bf4
Sapronov Konstantin <ks.costas@gmail.com>
2023-08-17 17:00:22 +0300

    feat: add config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,31 @@
+import {
+  DEFAULT_APP_PORT,
+  DEFAULT_LOG_LEVEL,
+  DEFAULT_MAX_FILE_SIZE,
+  DEFAULT_MAX_FILES,
+} from '../common/constants';
+
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 4000,
+  port: parseInt(process.env.PORT, 10) || DEFAULT_APP_PORT,
+
+  logger: {
+    maxFiles: process.env.LOG_MAX_FILES
+      ? parseInt(process.env.LOG_MAX_FILES, 10)
+      : DEFAULT_MAX_FILES,
+
+    maxFileSize: process.env.LOG_MAX_FILE_SIZE
+      ? parseInt(process.env.LOG_MAX_FILE_SIZE, 10) * 1024 // in Kb
+      : DEFAULT_MAX_FILE_SIZE * 1024,
+
+    levels: process.env.LOG_LEVELS
+      ? String(process.env.LOG_LEVELS).trim().split(' ')
+      : DEFAULT_LOG_LEVEL,
+  },
+
+  auth: {
+    accessSecret: process.env.JWT_SECRET_KEY || '',
+    accessExpiresIn: process.env.TOKEN_EXPIRE_TIME || '1h',
+    refreshSecret: process.env.JWT_SECRET_REFRESH_KEY || '',
+    refreshExpiresIn: process.env.TOKEN_REFRESH_EXPIRE_TIME || '1h',
+  },
 });

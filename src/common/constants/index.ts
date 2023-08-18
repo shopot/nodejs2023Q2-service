f8e24185e244c36a76650a307dfa50f83fe8807d
Sapronov Konstantin <ks.costas@gmail.com>
2023-08-18 11:26:45 +0300

    feat: add info

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -8,7 +8,8 @@ export const DEFAULT_MAX_FILE_SIZE = 100; // 100Kb
 
 export const DEFAULT_MAX_FILES = 5;
 
-export const DEFAULT_LOG_LEVEL: LogLevel[] = [
+// 'log' | 'error' | 'warn' | 'debug' | 'verbose'
+export const LOG_LEVELS_ARRAY: LogLevel[] = [
   'log',
   'error',
   'warn',
